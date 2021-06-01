e37564d8687c12b533e7122ba0430e14a032ea89
ChangSubKwak <paradise1732@gmail.com>
2021-06-01 20:51:50 +0900

    Add files for unit test using 'mocha'

---
+++ mocha_test/test/app.spec.js
@@ -0,0 +1,20 @@
+const sayHello = require('../app').sayHello;
+
+
+// without mocha
+if (sayHello) {
+    console.log('sayHello should return "hello"');
+    if (sayHello() === 'hello') {
+        console.log('Success');
+    } else {
+        console.log('Fail');
+    }
+}
+
+describe('App test!', function () {
+	it('sayHello should return hello', function (done) {
+			if (sayHello() === 'hello') {
+				done();
+			}
+	});
+});
