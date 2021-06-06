b60d41714b0216f634bf97c6d653a324030f6e82
ChangSubKwak <paradise1732@gmail.com>
2021-06-06 10:29:20 +0900

    Change filename and relevant works(number_baseball.html -> NumberBaseball.html)

--- 3_webpack_example/main.js
+++ 3_webpack_example/main.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'                               // package.json에 기록된 vue를 가져온다.
-import NumberBaseBall from './NumberBaseBall';
+import NumberBaseball from './NumberBaseball';
 
-new Vue(NumberBaseBall).$mount('#root');            // el역할을 $mount가 한다
+new Vue(NumberBaseball).$mount('#root');            // el역할을 $mount가 한다
