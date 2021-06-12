6c526992bf483787c3953ad0525bf0ef913f02a6
ChangSubKwak <paradise1732@gmail.com>
2021-06-12 21:17:13 +0900

    Add 'RockPaperScissors project initial files'

---
+++ 5_RockPaperScissors/webpack.config.js
@@ -0,0 +1,36 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path')
+
+module.exports = {
+	mode: 'development',
+	devtool: 'eval',                // 빌드속도가 빨라짐
+	resolve: {
+		extensions: ['.js', '.vue']         // import시 from 다음에 나오는 파일명의 확장자 제거 가능
+	},
+	//mode: 'production'
+	entry: {
+		//app: './main.js',         // 하나로 합쳐질 이름이 app
+		app: path.join(__dirname, './main.js'),           // 하나로 합쳐질 이름이 app
+	},
+	module: {
+		rules: [{                   // 어떻게 합칠 것인지를 정한다.
+			test: /\.vue$/,           // .vue로 끝나는 파일
+			use: 'vue-loader',
+		}, {
+			test: /\.css$/,           // .css로 끝나는 파일
+			use: [
+				'vue-style-loader',
+				'css-loader'
+			]
+		}
+		],
+	},
+	plugins: [
+		new VueLoaderPlugin(),      // module이 처리하면서 사용하는 도구
+	],
+	output: {
+		filename: 'app.js',
+		path: path.join(__dirname, 'dist'),      // __dirname is current folder name
+		publicPath: '/dist',
+	},
+};
