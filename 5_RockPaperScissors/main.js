de4a0e9b6b5e4f7f8af99d2ccbbef07b571be32c
ChangSubKwak <paradise1732@gmail.com>
2021-06-13 22:35:48 +0900

    Delete folder(.\5_RockPaperScissors)

--- 5_RockPaperScissors/main.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'                              		 // package.json에 기록된 vue를 가져온다.
-import RockPaperScissors from './RockPaperScissors';
-
-new Vue(RockPaperScissors).$mount('#root');            // el역할을 $mount가 한다
