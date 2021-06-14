e7f08f35587d76b5f609af97adf914a849de232b
ChangSubKwak <paradise1732@gmail.com>
2021-06-14 09:05:13 +0900

    Changing file content in 5_RockScissorsPaper

--- 5_RockScissorsPaper/RockScissorsPaper.vue
+++ 5_RockScissorsPaper/RockScissorsPaper.vue
@@ -1,11 +1,17 @@
 <template>
 	<div>
-		<div id="computer"></div>
+		<!-- <div id="computer" :class="{ state: true, hello: false }" :style="{ backgroundImage: '', fontSize: '14px' }"></div> -->
+		<!-- 아래와 같이 background-image처럼 쓸 수 없음 backgroundImage 만 인식함 -->
+		<!-- <div id="computer" :style="{ background-image: 'url(https://)' }"></div> -->
+		<!-- 만약 border-bottom-radius라면 borderBottomRadius로 써야함 -->
+		<div id="computer" :style="{ background: 'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) 0 0' }"></div>
 		<div>
-			<button>바위</button>
-			<button>가위</button>
-			<button>보</button>
+			<button @click="onClickButton('바위')">바위</button>
+			<button @click="onClickButton('가위')">가위</button>
+			<button @click="onClickButton('보')">보</button>
 		</div>
+		<div>{{result}}</div>
+		<div>\현재 {{result}}점</div>
 	</div>
 </template>
 
@@ -16,6 +22,9 @@ export default {
 	computed: {
 	},
   methods: {
+		onClickButton(choice) {
+			
+		}
   },
 };
 </script>
