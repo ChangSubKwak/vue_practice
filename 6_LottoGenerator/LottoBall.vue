81573b618a187bebe9ff4fbebfa089be91342f95
ChangSubKwak <paradise1732@gmail.com>
2021-06-24 07:57:27 +0900

    Add study contents for 'prop', 'components'

--- 6_LottoGenerator/LottoBall.vue
+++ 6_LottoGenerator/LottoBall.vue
@@ -5,14 +5,21 @@
 <script>
   export default {
     name: 'LottoBall',
-    props: ['number'],
+    props: ['number'],          // props는 read-only 같은 개념
     data() {
       let background;
       return {
         background,
-        numberData: this.number,
+        numberData: this.number,    // 이런 식으로 부모로 부터 온 데이터를 바꿀수는 있음
+                                    // 이렇게 하면 부모의 데이터와 자식의 데이터가 일치하지 않는 
+                                    // 현상이 발생함
       };
     },
+    methods: {
+      // changeNumber() {      // 부모로부터 받은 number은 못바꿈, 금지되는 방법
+      //   this.number = 5;
+      // },
+    }
   }
 </script>
 
--- 6_LottoGenerator/LottoGenerator.vue
+++ 6_LottoGenerator/LottoGenerator.vue
@@ -2,7 +2,7 @@
 	<div>
 		<div>당첨 숫자</div>
 		<div id="결과창">
-			<lotto-ball v-for="ball in winBalls"></lotto-ball>
+			<lotto-ball v-for="ball in winBalls" number="5"></lotto-ball>
 		</div>
 		<div>보너스</div>
 		<lotto-ball v-if="bonus"></lotto-ball>
@@ -11,25 +11,45 @@
 </template>
 
 <script>
-export default {
-  data() {
-		return {
-			winBalls: [],
-			bonus: null,
-			redo: false,
-		};
-  },
-	computed: {
-	},
-  methods: {
-	},
-	mounted() {
+	import LottoBall from './LottoBall.vue'
 
-	},
-	beforeDestory() {
-
+	function getWinNumbers() {
+		console.log('getWinNumbers');
+		const candidate = Array(45).fill().map((v, i) => i + 1);
+		const shuffle = [];
+		while (candidate.length > 0) {
+			shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
+		}
+		const bonusNumber = shuffle[shuffle.length - 1];
+		const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+		return [...winNumbers, bonusNumber];
 	}
-};
+
+	export default {
+		components: {
+			//'lotto-ball': LottoBall,
+			//'lotto-balls': LottoBall,		// 이렇게 하면 자동변환 안되어 template에서 테그를 lotto-balls로 써주어야 함
+			LottoBall,								// 이렇게 써도 됨, 케밥케이스를 파스칼케이스로 자동 변환해줌
+		},
+		data() {
+			return {
+				winNumbers: getWinNumbers(),
+				winBalls: [],
+				bonus: null,
+				redo: false,
+			};
+		},
+		computed: {
+		},
+		methods: {
+		},
+		mounted() {
+
+		},
+		beforeDestory() {
+
+		}
+	};
 </script>
 
 <style scoped>
