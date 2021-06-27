e319ee5d438a10ce792009b3c0c99ba33328a877
ChangSubKwak <paradise1732@gmail.com>
2021-06-28 01:48:28 +0900

    Add study contents for 'coloring ball', 'change parent value in child component'

--- 6_LottoGenerator/LottoBall.vue
+++ 6_LottoGenerator/LottoBall.vue
@@ -1,25 +1,62 @@
 <template>
-  <div class="ball" :style="{ background }">{{ numberData }}</div>
+  <!-- <div class="ball" :style="{ background }">{{ numberData }}</div> -->
+    <div class="ball" :style="{ background }">{{ number }}</div>
 </template>
 
 <script>
   export default {
     name: 'LottoBall',
-    props: ['number'],          // props는 read-only 같은 개념
+
+    props: ['number',               // 부모컴포넌트가 자식컴포넌트에게 물려주는 개념을 props라고 함
+      'changeFunc',                 // props는 read-only 같은 개념
+      'winBalls',
+    ],                              
+                                    
     data() {
-      let background;
       return {
-        background,
-        numberData: this.number,    // 이런 식으로 부모로 부터 온 데이터를 바꿀수는 있음
-                                    // 이렇게 하면 부모의 데이터와 자식의 데이터가 일치하지 않는 
-                                    // 현상이 발생함
+        // background,
+        //numberData: this.number,    // 이런 식으로 부모로 부터 온 데이터를 바꿀수는 있음
+                                      // 이렇게 해도 부모의 데이터는 변하지 않기에
+                                      // 부모와 자식의 데이터가 일치하지 않는 현상이 발생함
       };
     },
+
+    computed: {
+      styleObject() {
+        let background;
+      
+        if (this.number <= 10) {
+          background = 'red';
+        } else if (this.number <= 20) {
+          background = 'orange';
+        } else if (this.number <= 30) {
+          background = 'yellow';
+        } else if (this.number <= 40) {
+          background = 'green';
+        } else {
+          background = 'blue';
+        }
+
+        return { background };
+      }
+    },
+    
     methods: {
       // changeNumber() {      // 부모로부터 받은 number은 못바꿈, 금지되는 방법
       //   this.number = 5;
       // },
     },
+
+    mounted() {
+      if (this.winBalls && this.winBalls.length == 3) {
+        this.changeFunc(this.winBalls, 2, 100);         // 이렇게 하면 부모의 값과 자식의 값이 일치하게 됨
+      }
+    },
+    
+    beforedDestory() {
+      // setTimeout에 대하여 강제 종료를 여기서 함
+    }
+    
   }
 </script>
 
--- 6_LottoGenerator/LottoGenerator.vue
+++ 6_LottoGenerator/LottoGenerator.vue
@@ -3,11 +3,11 @@
     <div>당첨 숫자</div>
     <div id="결과창">
       <!-- <lotto-ball v-for="ball in winBalls" number="5"></lotto-ball> -->
-      <lotto-ball v-for="ball in winBalls" :key="ball">{{ ball }}</lotto-ball>
+      <lotto-ball v-for="ball in winBalls" :key="ball" :number="ball" :changeFunc="changeNumber" :winBalls="winBalls">{{ ball }}</lotto-ball>
     </div>
     <div>보너스</div>
-    <lotto-ball v-if="bonus"></lotto-ball>
-    <button v-if="redo">재시작</button>
+    <lotto-ball v-if="bonus" :number="bonus"></lotto-ball>
+    <button v-if="redo" @click="restart">재시작</button>
   </div>
 </template>
 
@@ -26,6 +26,7 @@
     return [...winNumbers, bonusNumber];		// 스프레드 오퍼레이터
   }
 
+  const timeouts = [];
   export default {
     components: {
       'lotto-ball': LottoBall,
@@ -51,34 +52,62 @@
         console.log('this.winBalls', this.winBalls);
 
         this.winNumbers[0] = 100;						// this.winNumbers[0]을 변경하면 this.winBalls[0]은 변경안됨
-        console.log('this.winNUmbers[0]', this.winNumbers[0]);
+        console.log('this.winNumbers[0]', this.winNumbers[0]);
         console.log('this.winBalls[0]', this.winBalls[0]);
 
         this.winBalls = this.winNumbers;		// 이렇게 하면 얕은 복사가 되어버림
-        console.log('this.winNUmbers', this.winNumbers);
+        console.log('this.winNumbers', this.winNumbers);
         console.log('this.winBalls', this.winBalls);
 
         this.winNumbers[0] = 200;						// this.winNumbers[0]을 변경하면 this.winBalls[0]도 변경됨
-        console.log('this.winNUmbers[0]', this.winNumbers[0]);
+        console.log('this.winNumbers[0]', this.winNumbers[0]);
         console.log('this.winBalls[0]', this.winBalls[0]);
       },
+
+      changeNumber(winBalls, idx, wantedBallValue) {
+        winBalls[idx]  = wantedBallValue;
+        // console.log(winBalls, idx, wantedBallValue);
+      },
+
+      restart() {
+        this.winNumbers = getWinNumbers();
+        this.winBalls = [];
+        this.bonus = null;
+        this.redo = false;
+        this.showBalls();
+      },
+
+      showBalls() {
+        for (let i = 0 ; i < this.winNumbers.length -1; i++) {
+          timeouts[i] = setTimeout(() => {
+            this.winBalls.push(this.winNumbers[i]);
+          }, (i + 1) * 1000);
+        }
+
+        timeouts[6] = setTimeout(() => {
+          this.bonus = this.winNumbers[6];
+          this.redo = true;
+        }, 7000);
+
+        timeouts[7] = setTimeout(() => {
+          console.log('Final Result', this.winBalls);
+        }, 8000);
+        
+      }
     },
+
     mounted() {
       // this.practiceDeepCopy();
-      for (let i = 0 ; i < this.winNumbers.length -1; i++) {
-        setTimeout(() => {
-          this.winBalls.push(this.winNumbers[i]);
-        }, (i + 1) * 1000);
-      }
-
-      setTimeout(() => {
-        this.bonus = true;
-        this.redo = true;
-      }, 7000);
+      this.showBalls();
+      // console.log('this.winBalls', this.winBalls);
+      // console.log('this.winBalls[6]', this.winBalls[6]);
     },
+    
     beforeDestory() {
-
-    }
+      timeouts.forEach((t) => {
+        clearTimeout(t);
+      })
+    },
   };
 </script>
 
