924c62560783fcabcb6a9ebe5e2a6a0b983df620
ChangSubKwak <paradise1732@gmail.com>
2021-07-02 23:52:41 +0900

    Add study contents for new project 'TicTacToe'

---
+++ 7_TicTacToe/TicTacToe.vue
@@ -0,0 +1,30 @@
+<template>
+  <table-component></table-component>
+</template>
+
+<script>
+  import TableComponent from './TableComponent';
+
+  export default {
+    components: {
+      TableComponent,
+    },
+    data() {
+      return {
+      };
+    },
+
+    computed: {
+    },
+    
+    methods: {
+    },
+
+    mounted() {
+    },
+    
+  }
+</script>
+
+<style>
+</style>
---
+++ 7_TicTacToe/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';															// package.json에 기록된 vue를 가져온다.
+import TicTacToe from './TicTacToe';
+
+new Vue(TicTacToe).$mount('#root');						// el역할을 $mount가 한다
