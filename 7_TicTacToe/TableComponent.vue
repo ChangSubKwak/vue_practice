df2bf6eda477fa932c4190aa78b786b5b79a2ded
ChangSubKwak <paradise1732@gmail.com>
2021-07-06 23:57:54 +0900

    Add study contents for initial setting 'TicTacToe'

---
+++ 7_TicTacToe/TableComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <table>
+    <tr-component></tr-component>
+  </table>
+</template>
+
+<script>
+import TrComponent from './Tromponent';
+
+export default {
+  components: {
+    TrComponent,
+  },
+};
+
+</script>
+
+<style>
+</style>
---
+++ 7_TicTacToe/TrComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <tr>
+    <td-component></td-component>
+  </tr>
+</template>
+
+<script>
+import TdComponent from './TdComponent';
+
+export default {
+  components: {
+    TdComponent,
+  },
+}
+
+</script>
+
+<style>
+</style>
